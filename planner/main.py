d58f5dec8f7f770d0908f3550e6fc6b1be1e2b28
Mohammad-Ab <root.alpha1@gmail.com>
2024-05-25 22:55:19 +0330

    set models and routes of event planner application

--- planner/main.py
+++ planner/main.py
@@ -1,1 +1,15 @@
-#this is main file for fastapi planner
+from fastapi import FastAPI 
+from routes.users import user_router
+from routes.events import event_router
+import uvicorn
+
+app = FastAPI()
+
+#Register routes
+app.include_router(user_router, prefix="/user")
+app.include_router(event_router, prefix="/event")
+
+@app.get("/")
+async def home():
+    return RedirectResponse(url="/event")
+
