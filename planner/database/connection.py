41b44fb73b7ffb55b504591b4236b09864c56729
Mohammad-Ab <root.alpha1@gmail.com>
2024-05-26 22:11:38 +0330

    update connection of database

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -2,6 +2,8 @@ from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
 from pydantic import BaseSetting
+from models.users import User
+from models.events import Event
 
 class Settings(BaseSetting):
     DATABASE_URL: Optional[str] = None
@@ -10,7 +12,7 @@ class Settings(BaseSetting):
         client = AsyncIOMotorClient(self.DATABASE_URL)
         await init_beanie(
             database=client.get_default_database(),
-            document_models=[]
+            document_models=[Event,User]
         )
 
     class Config:
