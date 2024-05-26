e4224263ba65153bbd7c10286b37a18033eda041
Mohammad-Ab <root.alpha1@gmail.com>
2024-05-26 17:54:59 +0330

    initialize beanie library to intract with mongodb

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -1,1 +1,17 @@
-#for handle database
+from beanie import init_beanie
+from motor.motor_asyncio import AsyncIOMotorClient
+from typing import Optional
+from pydantic import BaseSetting
+
+class Settings(BaseSetting):
+    DATABASE_URL: Optional[str] = None
+    
+    async def initialize_database(self):
+        client = AsyncIOMotorClient(self.DATABASE_URL)
+        await init_beanie(
+            database=client.get_default_database(),
+            document_models=[]
+        )
+
+    class Config:
+        env_file=".env"
