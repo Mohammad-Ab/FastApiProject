39ae0a21cf18a380be0399bace2fefd42c50ae17
Mohammad-Ab <root.alpha1@gmail.com>
2024-05-26 22:20:54 +0330

    add crud operations

--- planner/database/connection.py
+++ planner/database/connection.py
@@ -1,7 +1,7 @@
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 from typing import Optional
-from pydantic import BaseSetting
+from pydantic import BaseSetting,BaseModel
 from models.users import User
 from models.events import Event
 
@@ -17,3 +17,50 @@ class Settings(BaseSetting):
 
     class Config:
         env_file=".env"
+#now set crud operatins
+class Database:
+    
+    def __init__(self, model):
+        self.model = model
+    
+    #create
+    async def save(self,document) -> None:
+        await document.create()
+        return
+    
+    #read
+    async def get(self, id: PydanticObjectId) -> Any:
+        doc = await self.model.get(id)
+        if doc:
+            return doc
+        return False
+
+    async def get_all(self) -> List[Any]:
+        docs = await self.model.find_all().to_list()
+        return docs
+
+    #update
+     async def update(self, id: PydanticObjectId, body: BaseModel) -> Any:
+        doc_id = id
+        des_body = body.dict()
+
+        des_body = {k: v for k, v in des_body.items() if v is not None}
+        update_query = {"$set": {
+            field: value for field, value in des_body.items()
+        }}
+
+        doc = await self.get(doc_id)
+        if not doc:
+            return False
+        await doc.update(update_query)
+        return doc
+    
+    #delete
+    async def delete(self, id: PydanticObjectId) -> bool:
+        doc = await self.get(id)
+        if not doc:
+            return False
+        await doc.delete()
+        return True
+
+
